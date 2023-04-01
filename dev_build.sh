5a059cb0ca39138cb2083044bc32e937ce17e021
Zac Pearson <zacfpearson@gmail.com>
2023-04-01 17:31:02 -0500

    rename

--- dev_build.sh
+++ dev_build.sh
@@ -1,4 +1,4 @@
-sudo docker build -f docker/Dockerfile.dev -t sentiment-backend:dev docker
-sudo docker run -it --rm --mount type=bind,source=$(pwd)/code,target=/build sentiment-backend:dev /bin/bash -c "cargo build"
-sudo docker build --no-cache -f docker/Dockerfile.serve -t sentiment-backend:serve code
-sudo docker run --name sentiment-backend --rm --network=host sentiment-backend:serve
+sudo docker build -f docker/Dockerfile.dev -t horta-backend:dev docker
+sudo docker run -it --rm --mount type=bind,source=$(pwd)/code,target=/build horta-backend:dev /bin/bash -c "cargo build"
+sudo docker build --no-cache -f docker/Dockerfile.serve -t horta-backend:serve code
+sudo docker run --name horta-backend --rm --network=host horta-backend:serve
