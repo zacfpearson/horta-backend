6d7d0066720f9432df205bee7c987ea80afffd45
Zac Pearson <zacfpearson@gmail.com>
2023-04-09 11:58:38 -0500

    cleanup 3

--- code/src/main.rs
+++ code/src/main.rs
@@ -3,10 +3,7 @@ use futures_util::{FutureExt, StreamExt};
 use pretty_env_logger;
 use rand::prelude::*;
 use serde::{Deserialize, Serialize}; 
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex, Condvar};
-use std::thread;
-use std::time::Duration;
+use std::{collections::HashMap,sync::{Arc, Mutex, Condvar}, thread, time::Duration};
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use warp::{http::StatusCode, ws::{Message, WebSocket}, Filter, Rejection, Reply};
@@ -54,9 +51,9 @@ impl Game {
             numbers.push(number);
         }
 
-        //group inot person and computer cards.
+        //group into person and computer cards.
         let mut cards: Vec<Card> = Vec::new();
-        let split_numbers: Vec<&[u8]> = numbers.chunks(7).collect();
+        let split_numbers: Vec<&[u8]> = numbers.chunks(level as usize).collect();
         for number in split_numbers[0] {
             cards.push(Card::new(*number, Player::Computer));
         }   
@@ -91,26 +88,26 @@ impl Game {
 async fn games_handler(games: Games) -> Result<impl Reply> {
     //get random index
     let index = rand::thread_rng().gen_range(0..50);
-    if let Some(game_and_id) = games.lock().unwrap().get(&index){
+    let games = games.lock().unwrap(); 
+    if let Some(game_and_id) = games.get(&index){
         let uuid = Uuid::new_v4();
-        let encoded: Vec<u8> = bincode::serialize(&(uuid.as_u128(),index,game_and_id)).unwrap();
-        Ok(encoded)
-    } else {
-        Err(warp::reject::not_found())
+        if let Ok(encoded) = bincode::serialize(&(uuid.as_u128(),index,game_and_id)) {
+            return Ok(encoded)
+        } 
     }
+    Err(warp::reject::not_found())
 }
 
 pub async fn ws_handler(ws: warp::ws::Ws, _: u128, index: u64, games: Games) -> Result<impl Reply> {
-    if let Some(game) = games.lock().unwrap().get(&index) {
+    let games = games.lock().unwrap();
+    if let Some(game) = games.get(&index) {
         let game_clone = game.clone();
-        Ok(ws.on_upgrade(move |socket| game_connection(socket, game_clone)))
-    } else {
-        Err(warp::reject::not_found())
+        return Ok(ws.on_upgrade(move |socket| game_connection(socket, game_clone)))
     }
+    Err(warp::reject::not_found())
 }
 
 pub async fn game_connection(ws: WebSocket, game: Game) {
-    // Just echo all messages back...
     let (tx_ws, mut rx_ws) = ws.split();
 
     let (tx, rx) = mpsc::unbounded_channel();
@@ -129,12 +126,9 @@ pub async fn game_connection(ws: WebSocket, game: Game) {
     //the computer playing.
     let send_to_reciever = thread::spawn(move || {
         let (lock, cvar) = &*game_computer;
-        println!("have lock var");
         let mut game_instance = lock.lock().unwrap();
         while let Some(card) = game_instance.next_card(Player::Computer) {
             let current_difference = game_instance.current_difference(&card) as u64;
-
-            println!("have lock.Going to sleep for: {}", (current_difference));
             
             let sleep_time = if game_instance.next_card(Player::Person) == None {
                 400
@@ -142,24 +136,16 @@ pub async fn game_connection(ws: WebSocket, game: Game) {
                 current_difference * 2000
             };
 
-            println!("awake and ready.");
-
             let result = cvar.wait_timeout(game_instance, Duration::from_millis(sleep_time as u64)).unwrap();
-
-            println!("have result");
-
             game_instance = result.0;
 
             if result.1.timed_out() {
-                println!("timeout. Nex card: {}", card.number);
-                let encoded: Vec<u8> = bincode::serialize(&card).unwrap();
-                println!("Encoding");
-                game_instance.cards_played.push(card);
-                let _ = tx.send(Ok(Message::binary(encoded)));
-                println!("Sent!");
+                if let Ok(encoded) = bincode::serialize(&card) {
+                    game_instance.cards_played.push(card);
+                    let _ = tx.send(Ok(Message::binary(encoded)));
+                }
             }
         }
-        println!("All Done Sending");
     });
 
     while let Some(result) = rx_ws.next().await {
@@ -167,34 +153,24 @@ pub async fn game_connection(ws: WebSocket, game: Game) {
             Ok(msg) => msg,
             Err(e) => {
                 eprintln!("error receiving ws message for {}", e);
-                break; //handle differently.
+                break;
             }
         };
 
-        //TODO: idk if I want all of these prints. 
         if msg.is_binary() {
-            println!("deserializing card.");
             match bincode::deserialize(msg.as_bytes()) {
                 Ok(card) => {
                     let &(ref lock, ref cvar) = &*game_person;
-
-                    println!("getting card lock");
-                    let mut started = lock.lock().unwrap();
-
-                    started.cards_played.push(card);
-                    println!("set card.");
-                    // We notify the condvar that the value has changed.
-                    cvar.notify_all();    
-                    println!("sent.")
+                    let mut game = lock.lock().unwrap();
+                    if game.cards.contains(&card) {
+                        game.cards_played.push(card);
+                        cvar.notify_all();   
+                    }
                 },
                 Err(e) => {
                     println!("error deserializing card: {}", e);
                 }
             }
-        } else if msg.is_close() {
-            println!("closing connection!");
-        } else {
-            println!("{:?}", msg);
         }
     }
     
@@ -208,7 +184,7 @@ pub async fn health_handler() -> Result<impl Reply> {
 fn create_n_games(num_games: u64) -> Games {
     let mut games: HashMap<u64, Game> = HashMap::new();
     for idx in 0..num_games {
-        let new_game = Game::new(7); //create new game at level 7
+        let new_game = Game::new(7);
         games.insert(idx, new_game);
     }
 
