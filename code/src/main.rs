1d3e4be6a0040632f5f46a664f2951b936c64d8b
Zac Pearson <zacfpearson@gmail.com>
2023-04-09 22:33:46 -0500

    api path

--- code/src/main.rs
+++ code/src/main.rs
@@ -219,9 +219,13 @@ async fn main() {
         .and(games.clone())
         .and_then(ws_handler);
 
-    let routes = health_route
-        .or(card_routes)
-        .or(ws_route);
+    let routes = warp::path("horta").and(
+        warp::path("api").and(
+            health_route
+            .or(card_routes)
+            .or(ws_route)
+        )
+    );
 
     warp::serve(routes).run(([127, 0, 0, 1], 80)).await;
 }
